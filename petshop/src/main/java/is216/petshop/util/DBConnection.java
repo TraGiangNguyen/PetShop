49f14bf6a2f1e7e6c624e3b1f94f94a46335e902
Hoang Ai My <hoangaimy4617@gmail.com>
2026-05-07 00:12:56 +0700

    feat: hoan thanh module quan ly san pham

--- petshop/src/main/java/is216/petshop/util/DBConnection.java
+++ petshop/src/main/java/is216/petshop/util/DBConnection.java
@@ -1,26 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package is216.petshop.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-/**
- *
- * @author Admin
- */
+
 public class DBConnection {
+
     public static Connection getConnection() {
         try {
-        String url = "jdbc:mysql://localhost:3306/petstore";
-        String user = "petstore";
-        String password = "123456";
-
-        Connection conn = DriverManager.getConnection(url, user, password);
-        System.out.println("✅ Connected successfully!");
-        return conn;
-
+            String url = "jdbc:mysql://localhost:3306/petstore";
+            String user = "petstore";
+            String password = "123456";
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("✅ Connected successfully!");
+            return conn;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
