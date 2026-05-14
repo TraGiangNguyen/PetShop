5a6d402ab23687ed3b3a6df136052e6fc039c05c
24521502-ops <24521502@gm.uit.edu.vn>
2026-05-14 23:44:29 +0700

    FEAT: them chức năng nhan vien

---
+++ petshop/src/main/java/is216/petshop/model/NhanVienModel.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package is216.petshop.model;
+
+/**
+ *
+ * @author Trần Minh Quyền
+ */
+public class NhanVienModel {
+    private int maNhanVien;
+    private String hoTen;
+    private String chucVu; // Lấy từ bảng VAI_TRO
+    private String soDienThoai;
+    private String email;
+    private long luong; // Lấy từ bảng HO_SO_LUONG
+
+    public NhanVienModel() {
+    }
+
+    public NhanVienModel(int maNhanVien, String hoTen, String chucVu, String soDienThoai, String email, long luong) {
+        this.maNhanVien = maNhanVien;
+        this.hoTen = hoTen;
+        this.chucVu = chucVu;
+        this.soDienThoai = soDienThoai;
+        this.email = email;
+        this.luong = luong;
+    }
+
+    public int getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(int maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getLuong() {
+        return luong;
+    }
+
+    public void setLuong(long luong) {
+        this.luong = luong;
+    }
+}
--- petshop/src/main/java/is216/petshop/view/NhanVienPanel.java
+++ petshop/src/main/java/is216/petshop/view/NhanVienPanel.java
@@ -4,8 +4,8 @@
  */
 package is216.petshop.view;
 
-import is216.petshop.bus.NhanVienBUS;
-import is216.petshop.model.NhanVien;
+import is216.petshop.dao.NhanVienDAO;
+import is216.petshop.model.NhanVienModel;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -15,10 +15,10 @@ import java.util.List;
 public class NhanVienPanel extends JPanel {
     private JTable table;
     private DefaultTableModel tableModel;
-    private NhanVienBUS nhanVienBUS;
+    private NhanVienDAO nhanVienDAO;
 
     public NhanVienPanel() {
-        nhanVienBUS = new NhanVienBUS();
+        nhanVienDAO = new NhanVienDAO();
         initComponents();
         loadDataToTable();
     }
@@ -49,12 +49,12 @@ public class NhanVienPanel extends JPanel {
         btnAdd.setFont(new Font("Arial", Font.BOLD, 14));
         btnAdd.setPreferredSize(new Dimension(160, 40));
         btnAdd.putClientProperty("FlatLaf.style", "arc: 15");
-        
+
         headerPanel.add(titlePanel, BorderLayout.WEST);
         headerPanel.add(btnAdd, BorderLayout.EAST);
 
         // --- PHẦN BẢNG (JTABLE) ---
-        String[] columnNames = {"Họ tên", "Chức vụ", "Số điện thoại", "Email", "Lương", "Thao tác"};
+        String[] columnNames = { "Họ tên", "Chức vụ", "Số điện thoại", "Email", "Lương", "Thao tác" };
         tableModel = new DefaultTableModel(columnNames, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -62,7 +62,7 @@ public class NhanVienPanel extends JPanel {
             }
         };
         table = new JTable(tableModel);
-        
+
         // Tuỳ chỉnh giao diện bảng cho giống thiết kế
         table.setRowHeight(40);
         table.setShowVerticalLines(false);
@@ -82,17 +82,17 @@ public class NhanVienPanel extends JPanel {
 
     private void loadDataToTable() {
         tableModel.setRowCount(0); // Xoá dữ liệu cũ
-        List<NhanVien> list = nhanVienBUS.getDanhSachNhanVien();
+        List<NhanVienModel> list = nhanVienDAO.getDanhSachNhanVien();
         DecimalFormat formatter = new DecimalFormat("###,###,###đ");
-        
-        for (NhanVien nv : list) {
+
+        for (NhanVienModel nv : list) {
             Object[] row = {
-                nv.getHoTen(),
-                nv.getChucVu(),
-                nv.getSoDienThoai(),
-                nv.getEmail(),
-                formatter.format(nv.getLuong()), // Format tiền tệ
-                "✎  🗑" // Tạm dùng Unicode thay cho Icon. Trong thực tế bạn dùng JLabel chứa ImageIcon.
+                    nv.getHoTen(),
+                    nv.getChucVu(),
+                    nv.getSoDienThoai(),
+                    nv.getEmail(),
+                    formatter.format(nv.getLuong()), // Format tiền tệ
+                    "✎  🗑" // Tạm dùng Unicode thay cho Icon. Trong thực tế bạn dùng JLabel chứa ImageIcon.
             };
             tableModel.addRow(row);
         }
